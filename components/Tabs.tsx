7c2b95a85037067a5ff17258160846b7c462ff2f
andnh <hoagf.an99@gmail.com>
2022-09-20 15:21:36 +0700

    fix: conflict tab nft

--- components/Tabs.tsx
+++ components/Tabs.tsx
@@ -5,7 +5,6 @@ import Nftlayout from './Nftlayout'
 
 import { TokenType } from '../types/types'
 
-
 import Search from './Search'
 
 /* Assets */
@@ -55,26 +54,28 @@ const Tabs: React.FC<TabsProps> = ({ tabList, panels }) => {
 							classNames(
 								'w-full py-2.5 text-sm font-bold leading-5 text-blue-700',
 								'ring-white ring-opacity-25 ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-1',
-								selected ? 'bg-white shadow' : 'text-[#8E9BAE] hover:bg-blue-100/20'
+								selected
+									? 'bg-white shadow'
+									: 'text-[#8E9BAE] hover:bg-blue-100/20'
 							)
-
-
 						}
 					>
 						{tab}
-
 					</Tab>
 				))}
 			</Tab.List>
-			<Search placeholder='Seach token, NFT, ...' className='mb-7' />
+			<Search
+				placeholder='Seach token, NFT, ...'
+				className='mb-7'
+			/>
 			<Tab.Panels>
-
-				<TokenList tokens={tokenList} />
+				<Tab.Panel>
+					<TokenList tokens={tokenList} />
+				</Tab.Panel>
 				<Tab.Panel>
 					<Nftlayout />
 				</Tab.Panel>
-
-				
+			</Tab.Panels>
 		</Tab.Group>
 	)
 }
