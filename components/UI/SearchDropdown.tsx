d7ca06ad2ee914d12004e8bc8868a1963d8ed552
quanganh569 <quanganh569>
2022-09-20 17:54:08 +0700

    feat: create folder UI

---
+++ components/UI/SearchDropdown.tsx
@@ -0,0 +1,35 @@
+import Image from 'next/image'
+import React from 'react'
+
+import search from '../../assets/icons/search.svg'
+
+type SearchProps = {
+  placeholder?: string
+  className?: string
+  onChange?: (event: any) => void
+  onSubmit?: (event: any) => void
+  value?: string
+  searching?: boolean
+}
+
+const SearchDropdown: React.FC<SearchProps> = ({ placeholder, className, onChange, onSubmit, value }) => {
+  return (
+    <>
+      <div className={`flex rounded-lg border items-center gap-4 mb-3 px-2 ${className}`}>
+        <Image src={search} />
+        <div>
+          <input
+            id="first_name"
+            className=" w-64 text-gray-900 text-sm rounded-lg block w-full p-2.5"
+            type="search"
+            placeholder={placeholder}
+            onChange={onChange}
+            value={value}
+          />
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default SearchDropdown
