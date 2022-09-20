c3e7a557d673c5edd143250a3deecde77a3d5e39
Dang Hai <82139737+lazybee99bg@users.noreply.github.com>
2022-09-20 14:38:39 +0700

    build: add Nft, Nftlayout

--- components/Tabs.tsx
+++ components/Tabs.tsx
@@ -1,6 +1,7 @@
 import { Tab } from '@headlessui/react'
 import { type } from 'os'
 import React from 'react'
+import Nftlayout from './Nftlayout'
 import Search from './Search'
 
 type TabsProps<T> = {
@@ -25,23 +26,19 @@ const Tabs: React.FC = () => {
 							classNames(
 								'w-full py-2.5 text-sm font-bold leading-5 text-blue-700',
 								'ring-white ring-opacity-25 ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-1',
-								selected
-									? 'bg-white shadow'
-									: 'text-[#8E9BAE] hover:bg-blue-100/20'
+								selected ? 'bg-white shadow' : 'text-[#8E9BAE] hover:bg-blue-100/20'
 							)
-						}
-					>
+						}>
 						{category}
 					</Tab>
 				))}
 			</Tab.List>
-			<Search
-				placeholder='Seach token, NFT, ...'
-				className='mb-7'
-			/>
+			<Search placeholder='Seach token, NFT, ...' className='mb-7' />
 			<Tab.Panels>
 				<Tab.Panel>Tokens</Tab.Panel>
-				<Tab.Panel>NFT</Tab.Panel>
+				<Tab.Panel>
+					<Nftlayout />
+				</Tab.Panel>
 			</Tab.Panels>
 		</Tab.Group>
 	)
