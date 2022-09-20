cde0a22fcad3da03f9c26292f0e442af86a7dcb8
quanganh569 <quanganh569>
2022-09-20 17:54:08 +0700

    feat: create folder UI

---
+++ components/UI/Search.tsx
@@ -0,0 +1,20 @@
+import Image from 'next/image'
+import React from 'react'
+
+import search from '../../assets/icons/search.svg'
+
+type SearchProps = {
+  placeholder?: string
+  className?: string
+}
+
+const Search: React.FC<SearchProps> = ({ placeholder, className }) => {
+  return (
+    <div className={`flex items-center gap-4 p-4 ${className}`}>
+      <Image src={search} />
+      <input type="search" placeholder={placeholder} />
+    </div>
+  )
+}
+
+export default Search
