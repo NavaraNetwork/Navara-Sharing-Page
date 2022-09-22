8b96c92a24577f19425aed31f9c78223d8924da4
quanganh569 <quanganh569>
2022-09-22 16:59:41 +0700

    fix: off darkMode

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,26 @@
 module.exports = {
   content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      width: {
+        30: '7.5rem',
+        104: '26rem',
+        112: '28rem',
+        120: '30rem',
+        128: '32rem',
+        136: '34rem',
+        144: '36rem',
+      },
+      height: {
+        30: '7.5rem',
+        104: '26rem',
+        112: '28rem',
+        120: '30rem',
+        128: '32rem',
+        136: '34rem',
+        144: '36rem',
+      },
+    },
   },
   plugins: [],
   darkMode: 'class',
