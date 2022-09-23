ff4c8d200312af0d92b910a57314eab24f710c7d
Quang Anh Do <36727570+quanganh569@users.noreply.github.com>
2022-09-23 11:05:25 +0700

    feat: add redirect after search domain (#33)
    Co-authored-by: quanganh569 <quanganh569>

--- components/UI/SearchDropdown.tsx
+++ components/UI/SearchDropdown.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import { SearchIcon } from '@heroicons/react/solid'
+import { Input } from './Input'
 type SearchProps = {
   placeholder?: string
   className?: string
@@ -15,16 +16,9 @@ const SearchDropdown: React.FC<SearchProps> = ({ placeholder, className, onChang
     <>
       <div className={`flex items-center gap-4 -4  ${className}`}>
         <SearchIcon width="20" height="20" className="dark:text-white" />
-        {/* <Image src={search} className="p-1 w-10 h-10 rounded-full  " /> */}
+
         <div>
-          <input
-            id="first_name"
-            className="w-64 border no-zoom text-gray-900 text-sm rounded-lg block  p-2.5 dark:bg-white"
-            type="search"
-            placeholder={placeholder}
-            onChange={onChange}
-            value={value}
-          />
+          <Input placeholder="Search other domain" onChange={onChange} value={value} />
         </div>
       </div>
     </>
