dce9a425ba5656afae2bf9776e9d708796a3630c
andnh <hoagf.an99@gmail.com>
2022-09-23 16:27:16 +0700

    feat: add loading skeleton

--- utils/common.ts
+++ utils/common.ts
@@ -1,1 +1,4 @@
 export const merge = <T>(...objects: T[]) => objects.reduce((acc, cur) => ({ ...acc, ...cur }))
+export const classNames = (...classes: string[]) => {
+  return classes.filter(Boolean).join(' ')
+}
