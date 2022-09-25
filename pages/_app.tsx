db98cc9cc8325cf8e2e128cc54b6b4217a370653
quanganh569 <vlcheryl569@gmail.com>
2022-09-25 14:10:17 +0700

    feat: add darkmode on app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,11 @@
 import type { AppProps } from 'next/app'
 import '../styles/globals.css'
+import {ThemeProvider} from 'next-themes'
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    // <ThemeProvider enableSystem={true} attribute="class">
+    <ThemeProvider enableSystem={true} attribute="class">
     <Component {...pageProps} />
-    // </ThemeProvider>
+     </ThemeProvider>
   )
 }
 
