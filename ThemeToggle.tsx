af7db0f884e717f67d11a620fe3d412be80345eb
Quang Anh Do <36727570+quanganh569@users.noreply.github.com>
2022-09-27 15:04:41 +0700

    Quanganh (#40)
    * fix: icon inside SearchInput
    
    * feat: update UI Dropdown
    
    Co-authored-by: quanganh569 <quanganh569>

--- ThemeToggle.tsx
+++ ThemeToggle.tsx
@@ -9,14 +9,14 @@ const ThemeToggler = () => {
   if (!mounted) return null
   return (
     <button
-      className="w-8 h-8 bg-blue-100 rounded-lg dark:bg-slate-600 flex items-center justify-center hover:ring-2 ring-blue-400 transition-all duration-300 focus:outline-none"
+      className="w-8 h-8  rounded-lg dark:bg-slate-600 flex items-center justify-center hover:ring-2 ring-blue-400 transition-all duration-300 focus:outline-none"
       onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}
       aria-label="Toggle Dark Mode"
     >
       {theme === 'light' ? (
-        <MoonIcon className="text-blue-300 w-7 h-7" />
+        <MoonIcon className=" w-7 h-7" color="black" />
       ) : (
-        <SunIcon className="text-blue-400 w-7 h-7" />
+        <SunIcon className=" w-7 h-7" color="yellow" />
       )}
     </button>
   )
