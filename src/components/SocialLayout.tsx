99ee39bfcebc46cb499714ee8c8f7cf256b5f46d
quanganh569 <quanganh569>
2022-10-18 08:52:53 +0700

    feat: fix crash when build

--- src/components/SocialLayout.tsx
+++ src/components/SocialLayout.tsx
@@ -1,18 +1,15 @@
-import { useEffect } from 'react'
-import API from '../../services/api'
-
-export const SocialLayout = (props) => {
-  useEffect(() => {
-    async function fetchData() {
-      try {
-        const response = await API.get(`social-network?domain=${navara}`)
-        const json = await response
-        setPosts(json.data.children.map((it) => it.data))
-      } catch (e) {
-        console.error(e)
-      }
-    }
-    fetchData()
-  }, [])
+export const SocialLayout = (props: any) => {
+  //   useEffect(() => {
+  //     async function fetchData() {
+  //       try {
+  //         const response = await API.get(`social-network?domain=${navara}`)
+  //         const json = await response
+  //         setPosts(json.data.children.map((it) => it.data))
+  //       } catch (e) {
+  //         console.error(e)
+  //       }
+  //     }
+  //     fetchData()
+  //   }, [])
   return <div>SocialLayout</div>
 }
